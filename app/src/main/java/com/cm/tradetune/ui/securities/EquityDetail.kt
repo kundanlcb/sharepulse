05636230ffba3208aca5d00b1fe494cf1c7aef98
Kundan <kundanlcb@gmail.com>
2024-02-06 22:20:51 +0530

    working on EquityDetail

---
+++ app/src/main/java/com/cm/tradetune/ui/securities/EquityDetail.kt
@@ -0,0 +1,12 @@
+package com.cm.tradetune.ui.securities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cm.tradetune.R
+
+class EquityDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_equity_detail)
+    }
+}
