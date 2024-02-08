9216d1211a9135797e0bf09563dace5b5764ed71
Kundan <kundanlcb@gmail.com>
2024-02-08 17:34:35 +0530

    working on EquityDetail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,20 +37,21 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation("com.squareup.picasso:picasso:2.8")
     implementation("androidx.media3:media3-exoplayer:1.2.1")
     implementation("androidx.media3:media3-exoplayer-dash:1.2.1")
     implementation("androidx.media3:media3-ui:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
@@ -62,6 +63,8 @@ dependencies {
     implementation("io.reactivex.rxjava2:rxjava:2.2.21")
     implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     // Add Dagger dependencies using Kotlin DSL
     implementation("com.google.dagger:dagger:2.49")
--- app/src/main/java/com/cm/tradetune/ui/securities/EquityDetail.kt
+++ app/src/main/java/com/cm/tradetune/ui/securities/EquityDetail.kt
@@ -1,12 +1,61 @@
 package com.cm.tradetune.ui.securities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cm.tradetune.R
+import com.cm.tradetune.data.model.FeedDto
+import com.cm.tradetune.databinding.ActivityEquityDetailBinding
+import com.cm.tradetune.ui.home.home.FeedAdapter
 
 class EquityDetail : AppCompatActivity() {
+    private lateinit var binding: ActivityEquityDetailBinding
+    private lateinit var feedAdapter: FeedAdapter
+    private lateinit var viewModel: EquityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_equity_detail)
+        viewModel =
+            ViewModelProvider(this)[EquityViewModel::class.java]
+        binding = ActivityEquityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Set up the Toolbar
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Profile"
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_back) // Replace with your back arrow icon
+        }
+        setUpFeedList()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed() // Handle back button press
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpFeedList() {
+        // Initialize RecyclerView and its adapter
+        val emptyFeedList: List<FeedDto> = emptyList()
+        feedAdapter = FeedAdapter(supportFragmentManager,lifecycle,emptyFeedList)
+        // Set layout manager for the RecyclerView
+        binding.rvEquityPost.layoutManager = LinearLayoutManager(this)
+
+        // Set the adapter to the RecyclerView
+        binding.rvEquityPost.adapter = feedAdapter
+
+
+        // Observe LiveData from ViewModel
+        viewModel.getFeedListLiveData().observe(this) { feedList ->
+            // Update the adapter with the new data
+            feedAdapter.submitList(feedList)
+        }
     }
 }
